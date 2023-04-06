8775876952207bb789ce7a94e4018b14f9835862
Peter Yoachim <yoachim@uw.edu>
2023-04-06 11:52:40 -0700

    run all at once

---
+++ big_run/README.md
@@ -0,0 +1,2 @@
+Just a place to run lots of things at once.
+
---
+++ big_run/monster.sh
@@ -0,0 +1,23 @@
+python ../starter/starter.py --outDir ../starter
+python ../filter_swap/filters_uy.py --outDir ../filter_swap
+python ../filter_swap/filters_uz.py --outDir ../filter_swap
+python ../noroll/noroll.py --outDir ../noroll
+python ../roll_back_start/old_mjd0.py --outDir ../roll_back_start
+python ../roll_back_start/old_mjd0_uz.py --outDir ../roll_back_start
+python ../threeseasonroll/threeroll.py --outDir ../threeseasonroll
+python ../weather/weather.py --cloud_offset_year 0  --outDir ../weather
+python ../weather/weather.py --cloud_offset_year 1  --outDir ../weather
+python ../weather/weather.py --cloud_offset_year 2  --outDir ../weather
+python ../weather/weather.py --cloud_offset_year 4  --outDir ../weather
+python ../weather/weather.py --cloud_offset_year 6  --outDir ../weather
+python ../weather/weather.py --cloud_offset_year 8  --outDir ../weather
+python ../weather/weather.py --cloud_offset_year 10 --outDir ../weather
+python ../weather/weather.py --cloud_offset_year 12 --outDir ../weather
+python ../weather/weather.py --cloud_offset_year 14 --outDir ../weather
+python ../weather/weather.py --cloud_offset_year 16 --outDir ../weather
+python ../weather/weather.py --cloud_offset_year 18 --outDir ../weather
+python ../weather/weather.py --cloud_offset_year 20 --outDir ../weather
+python ../weather/weather.py --cloud_offset_year 30 --outDir ../weather
+python ../weather/weather.py --cloud_offset_year 31 --outDir ../weather
+python ../weather/weather.py --cloud_offset_year 35 --outDir ../weather
+python ../weather/weather.py --cloud_offset_year 36 --outDir ../weather
