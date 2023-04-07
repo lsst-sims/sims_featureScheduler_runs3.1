1e5f372b41a5fa33a7531d9c703927e80cfcc722
Peter Yoachim <yoachim@uw.edu>
2023-04-07 16:45:01 -0700

    reviving slefcal

--- big_run/monster.sh
+++ big_run/monster.sh
@@ -2,8 +2,9 @@ python ../starter/starter.py --outDir ../starter
 python ../filter_swap/filters_uy.py --outDir ../filter_swap
 python ../filter_swap/filters_uz.py --outDir ../filter_swap
 python ../noroll/noroll.py --outDir ../noroll
-python ../roll_back_start/old_mjd0.py --outDir ../roll_back_start
-python ../roll_back_start/old_mjd0_uz.py --outDir ../roll_back_start
+# Can't do these because they need ddf_grid.
+# python ../roll_back_start/old_mjd0.py --outDir ../roll_back_start
+# python ../roll_back_start/old_mjd0_uz.py --outDir ../roll_back_start
 python ../threeseasonroll/threeroll.py --outDir ../threeseasonroll
 python ../weather/weather.py --cloud_offset_year 0  --outDir ../weather
 python ../weather/weather.py --cloud_offset_year 1  --outDir ../weather
