98dc64f7bc69070df585448e2456eec8d4688e13
Peter Yoachim <yoachim@uw.edu>
2023-04-25 13:13:43 -0700

    uy1

---
+++ u_year1/README.md
@@ -0,0 +1,1 @@
+Make sure u-band closes in year 1, but after year 1 don't worry as much about template generation (in any filter).
---
+++ u_year1/uy1.sh
@@ -0,0 +1,6 @@
+python uy1.py --utw 12
+python uy1.py --utw 24
+python uy1.py --utw 50
+python uy1.py --utw 100
+python uy1.py --utw 150
+python uy1.py --utw 200
