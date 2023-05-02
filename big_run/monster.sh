0753c1dbb520eb58f3805415233e2492a58c3f1d
Peter Yoachim <yoachim@uw.edu>
2023-05-02 16:36:17 -0700

    fix monster

--- big_run/monster.sh
+++ big_run/monster.sh
@@ -2,9 +2,6 @@ python ../starter/starter.py --outDir ../starter
 python ../filter_swap/filters_uy.py --outDir ../filter_swap
 python ../filter_swap/filters_uz.py --outDir ../filter_swap
 python ../noroll/noroll.py --outDir ../noroll
-# Can't do these because they need ddf_grid.
-# python ../roll_back_start/old_mjd0.py --outDir ../roll_back_start
-# python ../roll_back_start/old_mjd0_uz.py --outDir ../roll_back_start
 python ../threeseasonroll/threeroll.py --outDir ../threeseasonroll
 python ../weather/weather.py --cloud_offset_year 0  --outDir ../weather
 python ../weather/weather.py --cloud_offset_year 1  --outDir ../weather
@@ -39,3 +36,6 @@ python ../u_year1/uy1.py --utw 50  --outDir ../u_year1
 python ../u_year1/uy1.py --utw 100 --outDir ../u_year1
 python ../u_year1/uy1.py --utw 150 --outDir ../u_year1
 python ../u_year1/uy1.py --utw 200 --outDir ../u_year1
+# Can't do these because they need ddf_grid.
+# python ../roll_back_start/old_mjd0.py --outDir ../roll_back_start
+# python ../roll_back_start/old_mjd0_uz.py --outDir ../roll_back_start
