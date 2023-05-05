c459bcbbc0d66394da4a3db744848695a919b9c7
Peter Yoachim <yoachim@uw.edu>
2023-05-05 12:58:14 -0700

    ug

---
+++ ug_year1/README.md
@@ -0,0 +1,2 @@
+Since u_year1 seemed to improve things, let's also fold g in there and see if we can make sure we 
+get good year1 coverage for that as well
---
+++ ug_year1/gen_space.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+if __name__ == "__main__":
+    weights = [12,24,50,75,100,125,150]
+    with open('ug.sh', 'w') as file:
+        for w1 in weights:
+            for w2 in weights:
+                file.write('python ug_y1.py --utw %i --utg %i \n' % (w1, w2), )
