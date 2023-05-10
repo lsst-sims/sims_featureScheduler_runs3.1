c98f7f8f10e452d024ab76112c3cc42f7c42dbbd
Peter Yoachim <yoachim@uw.edu>
2023-05-09 18:41:01 -0700

    sh

---
+++ twi_neo/twi_neo.sh
@@ -0,0 +1,14 @@
+python twi_neo.py --neo_am 2.8
+python twi_neo.py --neo_am 2.7
+python twi_neo.py --neo_am 2.6
+python twi_neo.py --neo_am 2.5
+python twi_neo.py --neo_am 2.4
+python twi_neo.py --neo_am 2.3
+python twi_neo.py --neo_am 2.2
+python twi_neo.py --neo_am 2.1
+python twi_neo.py --neo_am 2.0
+python twi_neo.py --neo_am 2.5 --neo_filters r
+python twi_neo.py --neo_am 2.5 --neo_filters i
+python twi_neo.py --neo_am 2.5 --neo_filters z
+python twi_neo.py --neo_am 2.5 --neo_filters iz
+python twi_neo.py --neo_am 2.5 --neo_filters ri
